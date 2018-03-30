6edc326e30aa73f03dd7151253b5b11c46b97c00
korbek100 <korbek100@gmail.com>
2018-03-30 18:53:15 -0300

    first commit

---
+++ TestUINavigationController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestUINavigationController
+//
+//  Created by Jose David Bustos H on 22-01-17.
+//  Copyright © 2017 Jose David Bustos H. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestUINavigationController/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TestUINavigationController
+//
+//  Created by Jose David Bustos H on 22-01-17.
+//  Copyright © 2017 Jose David Bustos H. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
